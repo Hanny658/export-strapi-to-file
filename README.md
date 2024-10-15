192521dcf730abc9743afe941ce182025f61c049
Hanny <zyhaierciyuan@gmail.com>
2024-10-15 16:59:36 +1100

    Configured .env and test passed

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Exporting a Collection Type to files
 Exporting a title and its fields from strapi backend to PDF/Excel 
 
+
+#### Need to have .env to record:
+- Your Backend Host
+- Your API Token
+
 //TODO
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv::dotenv().ok(); // Load .env
+
+    // Creat Endpoints from API and ENV
+    let strapi_endpoint = env::var("STRAPI_ENDPOINT").unwrap_or_else(|_| "https://api.do360.com/api".to_string());
+    let api_key = env::var("API_KEY").unwrap_or_else(|_| "Nope, API Key is NESSESARY!".to_string());
+    
+    println!("Endpoint is {} and API Key is {}.", strapi_endpoint, api_key);
 }
